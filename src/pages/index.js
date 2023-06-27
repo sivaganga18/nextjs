4485a7db74704e5d0c4f251ef01178415b36dc82
siva <siva@cartoonmango.com>
2023-06-27 12:25:34 +0530

    inital changes

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import { NextSeo } from "next-seo";
+
+const Index = ({ data, image }) => {
+  const title = `${data?.name} | Page details`;
+  return (
+    <div>
+      <NextSeo
+        title={title}
+        description={title}
+        openGraph={{
+          title: `${data?.name}`,
+          description: `${title}`,
+          images: [
+            {
+              url: image,
+              width: 1920,
+              height: 1080,
+              alt: "xx.s/jpg",
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "https://www.mintavibe.com/",
+        }}
+      />
+      <span>test adshj</span>
+    </div>
+  );
+};
+export default Index;
+
+export const getServerSideProps = async ({ query }) => {
+  // USING THIS GET URL PARAMETERS
+
+  const { id, image } = query;
+
+  return {
+    props: {
+      data: {
+        name: "siva",
+      },
+      image: "asdfda",
+    },
+  };
+};
